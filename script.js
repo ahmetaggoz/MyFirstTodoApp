b346c90cb86bc97005f832bacdccbf73f67e2d13
Ahmet Aggoz <ahmet.aggoz19@ogr.atauni.edu.tr>
2023-05-21 23:56:41 +0300

    cizikler yapildi

--- script.js
+++ script.js
@@ -1,26 +1,45 @@
-var list_items = document.querySelectorAll(".list-group-item");
+
+var gorev_listesi = [];
+if(localStorage.getItem("gorev_listesi") !== null){
+    gorev_listesi = JSON.parse(localStorage.getItem("gorev_listesi"))
+}
 
 
-
-list_items.forEach((items)=>{
-    items.addEventListener("click", ()=>{
-        if(items.classList.contains("cizik")){
-            items.classList.remove("cizik")
-        }else{
-            items.classList.add("cizik")
-        }
-    })
-})
-
+gorevleri_göster();
 var gorev_ekle_btn = document.querySelector("#gorev_ekle_btn");
 var gorev_gir = document.querySelector("#gorev_gir");
-var gorev_listesi = [];
+
 gorev_ekle_btn.addEventListener("click", (event)=>{
     gorev_listesi.push({
         id:gorev_listesi.length + 1,
         gorev_adi:gorev_gir.value,
         durum:"pending",
     })
+    gorev_gir.value = "";
+    localStorage.setItem("gorev_listesi", JSON.stringify(gorev_listesi))
+    gorevleri_göster();
     event.preventDefault();
     console.log(gorev_listesi)
 })
+
+function gorevleri_göster(){
+
+    listem = document.querySelector("#ul_gorevler");
+    listem.innerHTML = "";
+    for(let gorev of gorev_listesi){
+        let gorevlerim = `
+            <li class="list-group-item" onclick="yapildimi(this)" >
+                ${gorev.gorev_adi}
+            </li>
+        `
+        listem.insertAdjacentHTML("beforeend", gorevlerim)
+    }
+}
+
+function yapildimi(tiklandi){
+    if(tiklandi.classList.contains("cizik")){
+        tiklandi.classList.remove("cizik")
+    }else{
+        tiklandi.classList.add("cizik")
+    }
+}
