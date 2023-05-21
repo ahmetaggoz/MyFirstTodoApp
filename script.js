6c2ecfbaad0e8800aecba44f30169f3ac44b1a1c
Ahmet Aggoz <ahmet.aggoz19@ogr.atauni.edu.tr>
2023-05-21 23:06:55 +0300

    gorev listesi olusturuldu

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var list_items = document.querySelectorAll(".list-group-item");
 
+
+
 list_items.forEach((items)=>{
     items.addEventListener("click", ()=>{
         if(items.classList.contains("cizik")){
@@ -10,3 +12,15 @@ list_items.forEach((items)=>{
     })
 })
 
+var gorev_ekle_btn = document.querySelector("#gorev_ekle_btn");
+var gorev_gir = document.querySelector("#gorev_gir");
+var gorev_listesi = [];
+gorev_ekle_btn.addEventListener("click", (event)=>{
+    gorev_listesi.push({
+        id:gorev_listesi.length + 1,
+        gorev_adi:gorev_gir.value,
+        durum:"pending",
+    })
+    event.preventDefault();
+    console.log(gorev_listesi)
+})
